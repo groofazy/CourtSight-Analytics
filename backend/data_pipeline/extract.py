d9b144e30e34548975ff2ea1f5ffa65292846bf6
groofazy <gabe.gamboa05@gmail.com>
2026-01-07 12:07:44 -0800

    finished extraction logic

--- backend/data_pipeline/extract.py
+++ backend/data_pipeline/extract.py
@@ -1,13 +1,11 @@
 # batch extraction
 # calling swar_nba_api at the end of each day and updating existing data in our data warehouse
 
-import time
-import json
 import pandas as pd
 from nba_api.stats.endpoints import leaguedashplayerstats
 from nba_api.stats.static import teams
 
-def extract_all_active_players(season='2024-2025'):
+def extract_all_active_players(season='2024-25'):
     """
     Fetches stats for ALL active players in one big request
     """
@@ -18,12 +16,28 @@ def extract_all_active_players(season='2024-2025'):
         df = stats.get_data_frames()[0]
 
         # relevant columns needed for data
-        columns = ['PLAYER_ID', 'PLAYER_NAME', 'TEAM_ABBREVIATION', 
+        req_columns = ['PLAYER_ID', 'PLAYER_NAME', 'TEAM_ABBREVIATION', 
                    'AGE', 'GP', 'MIN', 
                    'FGM', 'FGA', 'FG_PCT', 
                    'FG3M', 'FG3A', 'FG3_PCT',
                    'FTA', 'FT_PCT', 
                    'OREB', 'DREB', 'REB',
                    'AST', 'TOV', 'STL', 'BLK',
-                   'PF', 'PFD', '']
+                   'PF', 'PFD', 
+                   'PTS']
+        raw_data = df[req_columns]
+
+        # save to layer
+        raw_data.to_json('raw_nba_stats.json', orient='records')
+        print(f"Succesfully extracted {len(raw_data)} players to raw_nba_stats.json")
+        return raw_data
+    
+    except Exception as e:
+        print(f"Extraction failed: {e}")
+        return None
+
+if __name__ == "__main__":
+    extract_all_active_players()
+        
+
 
